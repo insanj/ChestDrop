47cea132cc76c829ee9026c36e3f4742fbb402a4
insanj <insanj@gmail.com>
2011-08-04 15:25:31 -0400

    Removed some unnecessary comments.

--- me/insanj/ChestDrop/ChestDrop.java
+++ me/insanj/ChestDrop/ChestDrop.java
@@ -20,29 +20,6 @@
 			ChestDrop.java
 
 */
-/*
- Created by Julian Weiss (insanj), updates frequent on Google+ (and sometimes Twitter)!
-
- Please do not modify or decompile at any date, but feel free to distribute with credit.
- Production began on Tuesday, August 2nd, 2011.
- Last edited on: 8/2/11
-
- Pacific Northwest Tree Octopus Version 1.0!
- Special thanks to: 
- 		Camcade, Carlthealpaca, and Gonjigas for design and publicity and pretty much everything but the coding and whatnot.
-
- 		
- Works with the current CraftBukkit Build (#1000).
- All other information should be available at bukkit.org under The Pacific Northwest Tree Octopus.
-
- Currently supports:
-		Permissions plugin, version 3.1.6!
-
- THIS VERSION CURRENT HAS TWO CLASSES:
-			TreeOctopus.java
-			TreeOctopusListener.java
-
-*/
 
 package me.insanj.ChestDrop;
 
